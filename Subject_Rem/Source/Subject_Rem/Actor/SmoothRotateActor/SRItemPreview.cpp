8944ad79e49e725ba7d5890c206e14f5cbd28909
Jaejune <jjune0427@naver.com>
2025-07-20 19:48:18 +0900

    feat : Preview Item 주변 투명 처리 완료

---
+++ Subject_Rem/Source/Subject_Rem/Actor/SmoothRotateActor/SRItemPreview.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/SmoothRotateActor/SRItemPreview.h"
+#include "Components/SceneCaptureComponent2D.h"
+
+// Sets default values
+ASRItemPreview::ASRItemPreview()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>("SceneComponent");
+	SetRootComponent(SceneComponent);
+	SceneCaptureComponent2D = CreateDefaultSubobject<USceneCaptureComponent2D>("SceneCaptureComponent2D");
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+}
+
+// Called when the game starts or when spawned
+void ASRItemPreview::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASRItemPreview::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASRItemPreview::ReplaceStaticMesh(UStaticMesh* NewMesh)
+{
+	//메시 제거
+	if (!NewMesh)
+	{
+		MeshComp->SetStaticMesh(nullptr);
+		UE_LOG(LogTemp, Log, TEXT("Mesh cleared (set to nullptr)"));
+		return;
+	}
+
+	MeshComp->SetStaticMesh(NewMesh);
+	UE_LOG(LogTemp, Log, TEXT("Mesh replaced with: %s"), *NewMesh->GetName());
+}
---
+++ Subject_Rem/Source/Subject_Rem/Actor/SmoothRotateActor/SRItemPreview.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SRItemPreview.generated.h"
+
+class USceneComponent;
+class USceneCaptureComponent2D;
+
+UCLASS()
+class SUBJECT_REM_API ASRItemPreview : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASRItemPreview();
+	//어떤 StaticMesh로 랜더링할지 정함. nullptr을 넣으면 사라짐.
+	void ReplaceStaticMesh(UStaticMesh* NewMesh);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	USceneComponent* SceneComponent;
+	UPROPERTY(EditAnywhere)
+	USceneCaptureComponent2D* SceneCaptureComponent2D;
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
