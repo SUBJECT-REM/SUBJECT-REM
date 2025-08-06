708b94b2d43f197f801c24226c75eed5db1d3671
esq0604 <esq0604@gmail.com>
2025-08-06 18:54:58 +0900

    feat : 튜토리얼 컴포넌트 추가

---
+++ Subject_Rem/Source/Subject_Rem/Component/SRTutorialComponent.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/SRTutorialComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Actor/Tutorial/SRTutorialManager.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+USRTutorialComponent::USRTutorialComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+void USRTutorialComponent::NotifyTutorialObjective(FGameplayTag ObjectiveTag)
+{
+	if (TutorialManager)
+	{
+		if (TutorialCompletedDelaySeconds > 0)
+		{
+			FTimerHandle TutorialCompletedDelayHandle;
+			PendingObjectiveTag = ObjectiveTag;
+			GetWorld()->GetTimerManager().SetTimer(TutorialCompletedDelayHandle, this, &ThisClass::LateCompleteTutorialObjective, TutorialCompletedDelaySeconds, false);
+		}
+		else
+		{
+			TutorialManager->NotifyObjectiveCompleted(ObjectiveTag);
+		}
+
+	
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s - USRTutirualComponent :Tutorial Manager nullptr "), *this->GetName());
+	}
+}
+
+void USRTutorialComponent::SetTutorialWidgetComponent(UWidgetComponent* WidgetComp)
+{
+	OwnerTutorialWidgetComp = WidgetComp;
+}
+
+
+// Called when the game starts
+void USRTutorialComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TutorialManager = Cast<ASRTutorialManager>(UGameplayStatics::GetActorOfClass(GetWorld(), ASRTutorialManager::StaticClass()));
+
+	if (TutorialManager)
+	{
+		TutorialManager->OnTutorialStartDelegate.AddDynamic(this,&ThisClass::HandleTutorialStarted);
+		TutorialManager->OnTutorialCompleteDelegate.AddDynamic(this, &ThisClass::HandleTutorialCompleted);
+	}
+}
+
+void USRTutorialComponent::HandleTutorialStarted(FGameplayTag Tag)
+{
+	//Widget제거
+	if (OwnerTutorialWidgetComp && Tag== ExpectedTag)
+	{
+		OwnerTutorialWidgetComp->SetVisibility(true);
+	}
+}
+
+void USRTutorialComponent::HandleTutorialCompleted(FGameplayTag Tag)
+{
+	//Widget제거
+	if (OwnerTutorialWidgetComp && Tag == ExpectedTag)
+	{
+		OwnerTutorialWidgetComp->SetVisibility(false);
+	}
+}
+
+void USRTutorialComponent::LateCompleteTutorialObjective()
+{
+	if (TutorialManager && PendingObjectiveTag.IsValid())
+	{
+		TutorialManager->NotifyObjectiveCompleted(PendingObjectiveTag);
+		PendingObjectiveTag = FGameplayTag(); // 초기화
+
+	
+	}
+
+}
+
---
+++ Subject_Rem/Source/Subject_Rem/Component/SRTutorialComponent.h
@@ -0,0 +1,63 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameplayTagContainer.h"
+#include "Engine/TimerHandle.h"
+#include "SRTutorialComponent.generated.h"
+
+class ASRTutorialManager;
+class UWidgetComponent;
+
+/*튜토리얼 매니저를 참조해서 튜토리얼을 목적 태그를 바인딩할 컴포넌트 
+  액터의 튜토리얼 관련 위젯 컴포넌트가 있어야만 동작하는것이 아닌 Visible만 관리하는정도면 참조해도 괜찮다고 생각함(..?)
+  
+
+*/
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SUBJECT_REM_API USRTutorialComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USRTutorialComponent();
+
+	UFUNCTION(BlueprintCallable)
+	void NotifyTutorialObjective(FGameplayTag ObjectiveTag);
+
+	UFUNCTION(BlueprintCallable)
+	void SetTutorialWidgetComponent(UWidgetComponent* WidgetComp);
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void HandleTutorialStarted(FGameplayTag Tag);
+
+	UFUNCTION()
+	void HandleTutorialCompleted(FGameplayTag Tag);
+
+	UFUNCTION()
+	void LateCompleteTutorialObjective();
+
+	ASRTutorialManager* TutorialManager;
+		
+	UWidgetComponent* OwnerTutorialWidgetComp;
+
+	/** 지연 완료할 목표 태그 저장 */
+	FGameplayTag PendingObjectiveTag;
+
+	//튜토리얼 완료 지연 - 예) 움직이고 3초뒤 튜토리얼 완료
+	UPROPERTY(EditDefaultsOnly)
+	float TutorialCompletedDelaySeconds;
+
+	//튜토리얼 아이디
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag ExpectedTag;
+
+};
