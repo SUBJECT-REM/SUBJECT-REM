2764e708e875b3a11e96499d80b79024e427421b
esq0604 <esq0604@gmail.com>
2025-08-06 18:53:22 +0900

    feat : 튜토리얼 단계 태그 추가

--- Subject_Rem/Source/Subject_Rem/SRGameplayTags.cpp
+++ Subject_Rem/Source/Subject_Rem/SRGameplayTags.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "SRGameplayTags.h"
@@ -7,9 +7,12 @@ namespace SRGameplayTags
 {
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_ID_Move, "Tutorial.ID.Move", "Tutorial ID Tag");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_ID_PickupItem, "Tutorial.ID.PickupItem", "Tutorial ID Tag");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_ID_ClosePickupItemResult, "Tutorial.ID.ClosePickupItemResult", "Tutorial ID Tag");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_ID_ClosePickupItemResult, "Tutorial.ID.OepnInvenstigation", "Tutorial ID Tag");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_ID_ClickClueButton, "Tutorial.ID.ClickClueButton", "Tutorial ID Tag");
 
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_Objectives_Move, "Tutorial.Objectives.Move", "Tutorial Objectives Tag");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_Objectives_PickupItem, "Tutorial.Objectives.PickupItem", "Tutorial Objectives Tag");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_Objectives_ClosePickupItemResult, "Tutorial.Objectives.ClosePickupItemResult", "Tutorial Objectives Tag");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_Objectives_ClosePickupItemResult, "Tutorial.Objectives.OepnInvenstigation", "Tutorial Objectives Tag");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Tutorial_Objectives_ClickClueButton, "Tutorial.Objectives.ClickClueButton", "Tutorial Objectives Tag");
+
 }
