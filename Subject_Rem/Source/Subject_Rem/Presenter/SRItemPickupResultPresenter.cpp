9cecbbdf6f0b84b888c980ef3bcad8007e83246e
esq0604 <esq0604@gmail.com>
2025-08-06 18:54:26 +0900

    feat : 아이템 픽업 결과창 Presenter 추가

---
+++ Subject_Rem/Source/Subject_Rem/Presenter/SRItemPickupResultPresenter.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Presenter/SRItemPickupResultPresenter.h"
+#include "Component/SRInventoryComponent.h"
+#include "UI/SRItemPickupResultWidget.h"
+#include "SRItemData.h"
+#include "Blueprint/UserWidget.h"
+
+
+void USRItemPickupResultPresenter::Init(UActorComponent* InitComponent, UUserWidget* InitWidget)
+{
+	Super::Init(InitComponent, InitWidget);
+
+	InvenComp = Cast<USRInventoryComponent>(InitComponent);
+	if (!InvenComp)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InitComponent Cast cannot be cast to InventoryComponent"));
+		return;
+	}
+	check(InvenComp);
+
+	InvenComp->AddInventoryDataDelegate.AddDynamic(this, &ThisClass::ShowItemPickWidget);
+}
+
+void USRItemPickupResultPresenter::ShowItemPickWidget(const FSRItemBaseData& ShownItemData)
+{
+	if (!ItemPickupResultWidget)
+	{
+		//ItemPickupResultWidget에서는 Space입력시 닫아야함. 여기서 필요시 PlayerController를 넘겨줘야할거같음.
+		if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+		{
+			ItemPickupResultWidget = CreateWidget<USRItemPickupResultWidget>(PC, ItemPickUpResultWidgetClass);
+		}
+		check(ItemPickupResultWidget);
+	}
+	
+	ItemPickupResultWidget->SetItemName(ShownItemData.Name);
+	ItemPickupResultWidget->SetItemDes(ShownItemData.Description);
+
+	if (!ItemPickupResultWidget->IsInViewport())
+	{
+		ItemPickupResultWidget->AddToViewport(10);
+	}
+
+	if (ItemPickupResultWidget->GetVisibility() == ESlateVisibility::Hidden || ItemPickupResultWidget->GetVisibility() == ESlateVisibility::Collapsed)
+	{
+		ItemPickupResultWidget->SetVisibility(ESlateVisibility::Visible);
+	}
+}
+
+void USRItemPickupResultPresenter::HandleWidgetVisibilityChanged(ESlateVisibility NewVisibility)
+{
+	if (NewVisibility == ESlateVisibility::Visible)
+	{
+		if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+		{
+			PC->SetInputMode(FInputModeUIOnly());
+		}
+	}
+	else
+	{
+		if (APlayerController* PC = GetWorld()->GetFirstPlayerController())
+		{
+			PC->SetInputMode(FInputModeGameAndUI());
+		}
+	}
+}
+
+
---
+++ Subject_Rem/Source/Subject_Rem/Presenter/SRItemPickupResultPresenter.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Presenter/SRPresenter.h"
+#include "SRItemPickupResultPresenter.generated.h"
+
+/**
+ * 
+ */
+class USRInventoryComponent;
+class USRItemPickupResultWidget;
+UCLASS(Blueprinttype, Blueprintable)
+class SUBJECT_REM_API USRItemPickupResultPresenter : public USRPresenter
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Init(UActorComponent* InitComponent, UUserWidget* InitWidget) override;
+
+	UFUNCTION()
+	void ShowItemPickWidget(const FSRItemBaseData& ShownItemData);
+
+	UFUNCTION()
+	void HandleWidgetVisibilityChanged(ESlateVisibility NewVisibility);
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> ItemPickUpResultWidgetClass;
+
+	UPROPERTY()
+	USRInventoryComponent* InvenComp;
+
+	USRItemPickupResultWidget* ItemPickupResultWidget;
+};
