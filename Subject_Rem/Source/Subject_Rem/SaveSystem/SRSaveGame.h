1dde774d9850d1f45e60000a397b9b24695b5920
esq0604 <esq0604@gmail.com>
2025-10-12 09:50:50 +0900

    feat : 세이브 로드 1차, ground, basement 병합

---
+++ Subject_Rem/Source/Subject_Rem/SaveSystem/SRSaveGame.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SRSaveGame.generated.h"
+
+USTRUCT()
+struct FActorSaveData
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY()
+	FName ActorName;
+
+	UPROPERTY()
+	TArray<uint8> ByteData;
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class SUBJECT_REM_API USRSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+
+	/* 레벨에서 저장된 액터 (현재는 특정 레벨을 지원하지 않고 데모 맵을 가정합니다) */
+public:
+	UPROPERTY()
+	TMap<FName, FActorSaveData> SavedActorMap;
+};
